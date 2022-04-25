30425d144f11f5677b28001061928379dd2385b2
null-talisman <youngnilla@yahoo.com>
2022-04-24 22:43:06 -0500

    fixing indentation, removing vars, cleaning up

--- my_app/my_app.py
+++ my_app/my_app.py
@@ -12,44 +12,26 @@ def home():
 
 @app.route("/about")
 def about():
-    if request.method == "POST":
-        return render_template("about.html")
     return render_template("about.html")
 
 @app.route("/products")
 def products():
-    if request.method == "POST":
-        return render_template("products.html")
     return render_template("products.html")
 
 @app.route("/careers")
 def careers():
-    if request.method == "POST":
-        return render_template("careers.html")
     return render_template("careers.html")
 
 @app.route("/contact")
 def contact():
-    if request.method == "POST":
-        return render_template("contact.html")
     return render_template("contact.html")
 
 @app.route("/apply")
 def apply():
-    if request.method == "POST":
-        # getting input with references in HTML form
-        first_name = request.form.get("fname")
-        middle_initial = request.form.get("mname")
-        last_name = request.form.get("lname")
-        birth_date = request.form.get("birthdate")
-        phone_number = request.form.get("phonenumber")
-        email_address = request.form.get("emailaddress")
-        college = request.form.get("place")
-        degree = request.form.get("degree")
-        major = request.form.get("major")
-        minor = request.form.get("minor")
-        graddate = request.form.get("graddate")
-        return "Your name is " + first_name + last_name
+    """
+    TODO: Store application info as an "Application"
+    TODO: Store the application in a DB or simply send an email with it
+    """
     return render_template("application.html")
 
 
