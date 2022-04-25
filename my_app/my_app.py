732659b0a3006484c68df9abb29eace249a2d9ae
null-talisman <youngnilla@yahoo.com>
2022-04-24 19:47:57 -0500

    adding apply page

--- my_app/my_app.py
+++ my_app/my_app.py
@@ -1,12 +1,25 @@
-# simple web app to run in container
-
-from flask import Flask
+# simple web app
+from flask import Flask, request, render_template
 
 app = Flask(__name__)
 
-@app.route("/")
-def index():
-    return "Congratulations, it's a web app!"
+@app.route("/home")
+def home():
+    welcome_msg = """Welcome to SWA! \nPlease feel free to explore some of this app's functionality: \n"""
+    return welcome_msg
+
+@app.route("/apply")
+def apply():
+    if request.method == "POST":
+        # getting input with references in HTML form
+        first_name = request.form.get("fname")
+        middle_initial = request.form.get("mname")
+        last_name = request.form.get("lname")
+        birth_date = request.form.get("birthdate")
+        phone_number = request.form.get("phonenumber")
+        email_address = request.form.get("emailaddress")
+        return "Your name is " + first_name + last_name
+    return render_template("application.html")
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080, debug=False)
