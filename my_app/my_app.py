cf5b32c7e1e9ce0f5c08eaa0c00a2bf83c8a670a
null-talisman <youngnilla@yahoo.com>
2022-04-24 22:28:56 -0500

    adding more templates, finishing base site html

--- my_app/my_app.py
+++ my_app/my_app.py
@@ -3,10 +3,36 @@ from flask import Flask, request, render_template
 
 app = Flask(__name__)
 
+
 @app.route("/home")
 def home():
-    welcome_msg = """Welcome to SWA! \nPlease feel free to explore some of this app's functionality: \n"""
-    return welcome_msg
+    if request.method == "POST":
+        return render_template("home.html")
+    return render_template("home.html")
+
+@app.route("/about")
+def about():
+    if request.method == "POST":
+        return render_template("about.html")
+    return render_template("about.html")
+
+@app.route("/products")
+def products():
+    if request.method == "POST":
+        return render_template("products.html")
+    return render_template("products.html")
+
+@app.route("/careers")
+def careers():
+    if request.method == "POST":
+        return render_template("careers.html")
+    return render_template("careers.html")
+
+@app.route("/contact")
+def contact():
+    if request.method == "POST":
+        return render_template("contact.html")
+    return render_template("contact.html")
 
 @app.route("/apply")
 def apply():
@@ -18,8 +44,14 @@ def apply():
         birth_date = request.form.get("birthdate")
         phone_number = request.form.get("phonenumber")
         email_address = request.form.get("emailaddress")
+        college = request.form.get("place")
+        degree = request.form.get("degree")
+        major = request.form.get("major")
+        minor = request.form.get("minor")
+        graddate = request.form.get("graddate")
         return "Your name is " + first_name + last_name
     return render_template("application.html")
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080, debug=False)
